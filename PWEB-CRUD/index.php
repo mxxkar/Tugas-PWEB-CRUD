05e30b5c2dbedeb1dfd18e147091d0c1c2bbaa3f
Sekar <90591077+mxxkar@users.noreply.github.com>
2023-12-11 11:28:46 +0700

    Add files via upload

---
+++ PWEB-CRUD/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pendaftaran Siswa Baru | SMK Coding</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container-fluid mt-5">   
+    <header>
+        <h3>Pendaftaran Siswa Baru</h3>
+        <h1>SMA Al-Azhar 1 Jakarta</h1>
+    </header>
+
+    <h4>Menu</h4>
+    <nav>
+        <ul>
+            <li><a href="form-daftar.php">Daftar Baru</a></li>
+            <li><a href="list-siswa.php">Pendaftar</a></li>
+        </ul>
+    </nav>
+
+    <?php if(isset($_GET['status'])): ?>
+    <p>
+        <?php
+            if($_GET['status'] == 'sukses'){
+                echo "Pendaftaran siswa baru berhasil!";
+            } else {
+                echo "Pendaftaran gagal!";
+            }
+        ?>
+    </p>
+
+    <?php endif; ?>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    </body>
+</html>
